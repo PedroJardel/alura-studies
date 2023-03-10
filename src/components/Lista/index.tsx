ca20b06281281eda1ff65f256d2aa12fd3962d2f
Pedro Lima <pedrozagolima@gmail.com>
2023-03-10 00:22:53 -0300

    Created the css in the components 'Formulario','Botao','Lista','pages'

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,9 +1,34 @@
-import React from  'react';
+import React from 'react';
+import './style.scss'
 
-const Lista = () => {
+function Lista() {
+    //Criando um vetor de objetos para renderizar de forma
+    //dinâmica no React
+    const tarefas = [{
+        tarefa: 'React',
+        tempo: '02:00:00'
+    }, {
+        tarefa: 'Javascript',
+        tempo: '01:00:00'
+    }, {
+        tarefa: 'Typescript',
+        tempo: '03:00:00'
+    }]
     return (
-        <aside>
-            
+        <aside className='listaTarefas'>
+            <h2> Estudos do dia</h2>
+            <ul>
+                {tarefas.map((item, index) => (
+                    <li key={index} className='item'>
+                        <h3>
+                            {item.tarefa}
+                        </h3>
+                        <span>
+                            {item.tempo}
+                        </span>
+                    </li>
+                ))}
+            </ul>
         </aside>
     )
 }
